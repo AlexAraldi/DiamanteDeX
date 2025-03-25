b9ed97aff2db16aa9139c2679abad55c3421153c
AlexAraldi <alexanderaraldi@gmail.com>
2025-03-25 17:10:57 -0300

    Implementado condição de número ímpar e estrutura de fatorial do número digitado

--- DiamanteDeX/Program.cs
+++ DiamanteDeX/Program.cs
@@ -9,13 +9,43 @@
         // A linha central do diamante deve conter o número máximo de 'X's.
         static void Main(string[] args)
         {
+            Console.Clear();
             Console.WriteLine("Digite um número ímpar:   ");
             int numImp = Convert.ToInt32(Console.ReadLine());
 
-            while ()
+
+            while (numImp % 2 == 0)
+            {
+                Console.WriteLine("O número é invalido digite novamente!!!!!");
+                Console.WriteLine("Digite um número ímpar:   ");
+                numImp = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine($"O seu numero impar é: {numImp} ");
+
+            int fatorial = 1;
+            
+            for (int i = numImp; i >= 1; i--)
+            {
+                Console.Write( " ");
+
+                fatorial *= i;
+
+                if (i > 1)
+
+                {
+                    Console.Write("X ");
+                }
+
+            }
 
 
-                Console.ReadLine();
+
+
+            Console.ReadLine();
+
+
+
         }
     }
 }
