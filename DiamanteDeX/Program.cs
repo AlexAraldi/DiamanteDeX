df655dbf71405734f2cd0a20a7b35510cef6f39d
AlexAraldi <alexanderaraldi@gmail.com>
2025-03-25 15:57:31 -0300

    Add project files.

---
+++ DiamanteDeX/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace DiamanteDeX
+{
+    internal class Program
+    {
+        // Desenvolver um programa em que, dado um número ímpar como entrada, exibirá um diamante desenhado com a letra 'X'.
+        // O programa deve solicitar ao usuário um número inteiro.
+        // O número deve ser ímpar.Caso contrário, o programa deve informar que entrada é inválida e solicitar um novo número.
+        // O número de linhas e colunas deve ser igual ao número ímpar fornecido.
+        // A linha central do diamante deve conter o número máximo de 'X's.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número ímpar:   ");
+            int numImp = Convert.ToInt32(Console.ReadLine());
+
+            while ()
+
+
+                Console.ReadLine();
+        }
+    }
+}
